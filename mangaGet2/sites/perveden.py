627f80ce53ff1c8fc34fcac16930ad91e418adb1
Christopher Jackson <darkdragn.cj@gmail.com>
2016-08-12 14:26:09 -0700

    Removal of 'ADULT' site support from main branch.
    This reverts commit 6cdd5bb7180022369ae5c414535c526e73b40878.
        Making this branch 'CLEAN'

--- mangaGet2/sites/perveden.py
+++
@@ -1,6 +0,0 @@
-from mangaeden import mangaeden
-
-class perveden(mangaeden):
-    tags = [ 'pe', 'perveden' ]
-    siteTemplate = 'http://www.perveden.com{}'
-    searchTemplate = siteTemplate.format('/en-directory/?title={}')
