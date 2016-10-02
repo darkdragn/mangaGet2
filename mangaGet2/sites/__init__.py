5486364f21e076272b9b02ade8db56d5ad669500
Christopher Jackson <darkdragn.cj@gmail.com>
2016-10-01 19:49:48 -0500

    included h2r and rcb to __init__ for sites

--- mangaGet2/sites/__init__.py
+++ mangaGet2/sites/__init__.py
@@ -1,3 +1,5 @@
-from . import comicastle, mangaeden, mangahere, mangapark
+from . import comicastle, mangaeden, mangahere, mangapark, readcomicbooksonline
+from . import hentai2read
 
-__all__ = ['comicastle', 'mangaeden', 'mangahere', 'mangapark']
+__all__ = ['comicastle', 'hentai2read', 'mangaeden', 'mangahere', 'mangapark',
+           'readcomicbooksonline']
